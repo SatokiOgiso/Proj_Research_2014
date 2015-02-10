9dbd45e11c01b8d41a77e024bb6e3e2fe3a130ce
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-10 17:29:08 +0900

    deleted non-functional process regarding LEDs

--- P_2.ino
+++ P_2.ino
@@ -7,10 +7,8 @@ char Flag =  0;
 char Count = 0;
 char sendFlag = 0;
 int P_Time = 0;
-int LedCount = 0;
 unsigned long Time;
 unsigned long StartTime;
-unsigned long LedTime = 0;
 unsigned char KeyNo = 2;
 unsigned char RecNo = 0;
 unsigned char MyDeviceNo = 2;   // 自局の識別デバイス番号
@@ -30,20 +28,17 @@ void setup()
  *  繰り返し実行される処理の関数(メインの処理)                                  *
  *******************************************************************************/
 void loop(){
-  if(Flag == 0 && Serial.read() == 'A' ){
+  if(Flag == 0 && Serial.read() == 'A' ){ // 最初だけ送る
     Serial.println("START.");
     StartTime = micros();
     Flag = 1;
   }
-  else if(Flag == 1){
+  else if(Flag == 1){ 
     if(digitalRead(8) == LOW){
       Time = micros();
       while(digitalRead(8) == LOW);
       Time = micros() - Time;
-      if(Time > 3000 && Time <= 5000 && LedCount < 50){
-        if( micros() - LedTime < 100000 ){
-          LedCount++;
-        }
+      if(Time > 3000 && Time <= 5000 ){
         Count = 1;
       }
       if(Count ==1){
@@ -51,7 +46,6 @@ void loop(){
         P_Time = (micros() - StartTime)/1000000;
         Serial.print("B\t");
         Serial.println(P_Time);
-        LedTime = micros();
         delay(50);
       }
       Count = 0;
@@ -60,14 +54,7 @@ void loop(){
       Time = micros() - Time;
       if(Time > 1000000 ){
         digitalWrite(13,LOW);
-        LedCount = 0;
       }
-    }
   }
 }
-
-
-
-
-
 
