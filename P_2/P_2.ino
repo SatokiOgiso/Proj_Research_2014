fa9c9fc4d2974f230e99424673f976a3887e093c
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-10 17:41:46 +0900

    more refactored. dead flag was demolished

--- P_2/P_2.ino
+++ P_2/P_2.ino
@@ -1,6 +1,5 @@
 #include <Prescaler.h>
 
-char Flag =  0;
 int P_Time = 0;
 unsigned long Time;
 unsigned long StartTime;
@@ -13,34 +12,30 @@ void setup()
   pinMode(13,OUTPUT);
   pinMode(8,INPUT);
   digitalWrite(13,LOW);
+  while(Serial.read() != 'A');
+  Serial.println("START.");
+  StartTime = micros();
 }
 /*******************************************************************************
  *  繰り返し実行される処理の関数(メインの処理)                                  *
  *******************************************************************************/
 void loop(){
-  if(Flag == 0 && Serial.read() == 'A' ){ // 最初だけ送る
-    Serial.println("START.");
-    StartTime = micros();
-    Flag = 1;
-  }
-  else if(Flag == 1){ 
-    if(digitalRead(8) == LOW){
-      Time = micros();
-      while(digitalRead(8) == LOW);
-      Time = micros() - Time;
-      if(Time > 3000 && Time <= 5000 ){
-        digitalWrite(13,HIGH);
-        P_Time = (micros() - StartTime)/1000000;
-        Serial.print("B\t");
-        Serial.println(P_Time);
-        delay(50);
-      }
+  if(digitalRead(8) == LOW){
+    Time = micros();
+    while(digitalRead(8) == LOW);
+    Time = micros() - Time;
+    if(Time > 3000 && Time <= 5000 ){
+      digitalWrite(13,HIGH);
+      P_Time = (micros() - StartTime)/1000000;
+      Serial.print("B\t");
+      Serial.println(P_Time);
+      delay(50);
     }
-    else if(digitalRead(8) == HIGH){
-      Time = micros() - Time;
-      if(Time > 1000000 ){
-        digitalWrite(13,LOW);
-      }
+  }
+  else if(digitalRead(8) == HIGH){
+    Time = micros() - Time;
+    if(Time > 1000000 ){
+      digitalWrite(13,LOW);
     }
   }
 }
