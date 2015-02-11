813e8b594d8342dad0051c08e23067c9907aa225
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-12 01:38:58 +0900

    modified to handle str to function

--- GUI/recvProcess.py
+++ GUI/recvProcess.py
@@ -1,7 +1,9 @@
 
 
-def parseData():
-    return 1
+def parseData(obtained_str):
+    #please write your code here
+    retval = 1 # temporary
+    return retval
 
 
 
--- GUI/recvProcessTest.py
+++ GUI/recvProcessTest.py
@@ -6,62 +6,62 @@ class TestRecvProcess(unittest.TestCase):
     #普通のデータたち．Aに続く値を返す
     def testData1(self):
         obtained_char = 'A1\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = 1
         self.assertEqual(e_obtained, e_expected)
 
     def testData100(self):
         obtained_char = 'A100\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = 100
         self.assertEqual(e_obtained, e_expected)
 
     def testData200(self):
         obtained_char = 'A200\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = 200
         self.assertEqual(e_obtained, e_expected)
         
     def testData1000(self):
         obtained_char = 'A1000\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = 1000
         self.assertEqual(e_obtained, e_expected)
 
     #ありえない値たち．空リスト [] を返す
     def testData_miss1(self):
         obtained_char = 'B1000\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
         
     def testData_miss2(self):
         obtained_char = 'A10000\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
 
     def testData_miss3(self):
         obtained_char = 'A\n'
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
 
     def testData_miss4(self):
         obtained_char = 'A100' #改行がない場合
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
         
     def testData_miss5(self):
         obtained_char = '' 
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
         
     def testData_miss6(self):
         obtained_char = '1r0ff' #不明な値たち
-        e_obtained = recvProcess.parseData()
+        e_obtained = recvProcess.parseData(obtained_char)
         e_expected = []
         self.assertEqual(e_obtained, e_expected)
 
