5da9b189cca2e5b27110e6aab18456e96a4eba79
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-12 02:16:52 +0900

    added serial read and parse with \n

--- GUI/presenGUI.py
+++ GUI/presenGUI.py
@@ -1,14 +1,25 @@
-
+import serial 
 
 
 def main():
+    ser = serial.Serial(port = 'COM8', baudrate = 9600) # serial port initialization
+    recvWord = []
+    
     try:
         while True:
-            print 'hello'
-            
+            while ser.inWaiting() > 0:
+                recvWord.append(ser.read(1))
+                if recvWord[-1] == '\n': # if the word was completed
+                    dataset = "".join(recvWord)
+                    print dataset
+                    recvWord = []
+                    
     except KeyboardInterrupt:
         print 'end'
             
+    ser.close()
+    print 'serial port closed'
+
             
 if __name__ == '__main__':
     main()
