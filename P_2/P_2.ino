6626320d507533f12c960f8653485aca37f908b6
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-02-11 12:29:16 +0900

    added some led related process

--- P_2/P_2.ino
+++ P_2/P_2.ino
@@ -3,6 +3,12 @@
 int P_Time = 0;
 unsigned long Time;
 unsigned long StartTime;
+
+unsigned int led_value = 0;
+
+int LED = 9
+
+
 /*******************************************************************************
  *  電源起動時とリセットの時だけのみ処理される関数(初期化と設定処理)            *
  *******************************************************************************/
@@ -11,16 +17,54 @@ void setup()
   Serial.begin(9600) ;	// ArduinoIDEと通信する為の初期化処理
   pinMode(13,OUTPUT);
   pinMode(8,INPUT);
+  pinMode(LED, OUTPUT);
   digitalWrite(13,LOW);
+  analogWrite(LED, 0);
 //  while(Serial.read() != 'A');
   Serial.println("START.");
   StartTime = micros();
 }
 
+
+unsigned int led_update(unsigned int led_value, int prescale_value, int stepsize)
+{
+  unsigned long Now;
+  static unsigned long LastUpdateTime = 0;
+  
+  Now = micros();
+  
+  if((Now - LastUpdateTime) > prescale_value){
+    led_value = led_value + stepsize;
+    LastUpdateTime = Now;
+  }
+  
+  return led_value
+}
+
+unsigned int led_validate(unsigned int led_value)
+{
+  unsigned long Now;
+  static unsigned long LastLEDChange = 0;
+  
+  Now = micros();
+  
+  
+  
+  if(led_value > 255)  led_value = 255;
+  if(led_value < 0)    led_value = 0;
+  
+  return led_value
+}
+
+
 /*******************************************************************************
  *  繰り返し実行される処理の関数(メインの処理)                                  *
  *******************************************************************************/
 void loop(){
+  
+  led_value = led_update(led_value, 100000, 1);
+  analogWrite(LED, led_validate(led_value));
+  
   if(digitalRead(8) == LOW){
     Time = micros();
     while(digitalRead(8) == LOW);
